afdae688134cc00e6a5f3726f77ca4cede3cb898
Ratu <ratukf@gmail.com>
2024-03-06 15:32:45 +0700

    Laman user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@
 
 // Variabel global
 var activePage;
+var email;
+var password;
 
 // Fungsi untuk menampilkan login user atau admin
 function showLoginContainer(userType) {
@@ -18,11 +20,69 @@ function showLoginContainer(userType) {
     console.log("active page: ", activePage);
 }
 
-function back(activePage) {
+function back() {
     document.getElementById("home").style.display = "block";
     if (activePage == "loginUser") {
         document.getElementById("containerLoginUser").style.display = "none";
-    } else {
+    } else if (activePage == "loginAdmin") {
         document.getElementById("containerLoginAdmin").style.display = "none";
     }
 }
+
+function login(userType) {
+    if (userType == "user") {
+        email = document.getElementById("emailUser").value;
+        password = document.getElementById("passwordUser").value;
+        if (email && password) {
+            document.getElementById("containerLoginUser").style.display = "none";
+            window.location.href = "../pages/lamanUser.html";
+        } else {
+            console.log("Kolom email dan password harus diisi");
+        }
+    } else {
+        email = document.getElementById("emailAdmin").value;
+        password = document.getElementById("passwordAdmin").value;
+        if (email && password) {
+            document.getElementById("containerLoginAdmin").style.display = "none";
+            window.location.href = "../pages/lamanAdmin.html";
+        } else {
+            console.log("Kolom email dan password harus diisi");
+        }
+    }
+}
+
+function showLamanUser(div) {
+    var elements = ['homeLamanUser', 'profilLamanUser', 'dataPegawaiLamanUser', 'akunLamanUser'];
+
+    elements.forEach(function(elementID) {
+        document.getElementById(elementID).style.transition = 'opacity 0.1s ease';
+        document.getElementById(elementID).style.opacity = '0';
+    });
+
+    setTimeout(function() {
+        elements.forEach(function(elementID) {
+            document.getElementById(elementID).style.display = 'none';
+        });
+
+        if (div == 'profil') {
+            document.getElementById('profilLamanUser').style.display = 'block';
+            document.getElementById('profilLink').style.textDecoration = 'underline';
+        } else if (div == 'dataPegawai') {
+            document.getElementById('dataPegawaiLamanUser').style.display = 'block';
+            document.getElementById('dataPegawaiLink').style.textDecoration = 'underline';
+        } else if (div == 'akun') {
+            document.getElementById('akunLamanUser').style.display = 'block';
+            document.getElementById('akunLink').style.textDecoration = 'underline';
+        } else {
+            document.getElementById('homeLamanUser').style.display = 'block';
+            document.getElementById('homeLink').style.textDecoration = 'underline';
+        }
+
+        setTimeout(function() {
+            elements.forEach(function(elementID) {
+                document.getElementById(elementID).style.opacity = '1';
+            });
+        }, 20);
+    }, 100);
+}
+
